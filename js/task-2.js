e3b5d85700ccb959a9eb18cc3b4fa7d8fb74bef7
VadymVlodarchyk <vadymvlodarchyk@gmail.com>
2023-11-26 22:00:58 +0000

    nbv

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,17 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const deliveryMessage = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return deliveryMessage;
-}
+// function formatMessage(message, maxLength) {
+//     if (message.length <= maxLength) {
+//         return message;
+//     } else {
+//         message = message.slice(0, maxLength) + '...';
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20));
+//         return message;
+//     }
+
+    
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16));
+// console.log(formatMessage("Curabitur ligula sapien", 23)); 
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
